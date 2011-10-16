8c930c54d78719f3e9330bee0729df959e6e214c
Andy <amastbaum@gmail.com>
2011-10-16 02:34:50 -0400

    minor bugfix. prevent problem creating doc from messing up list page

--- lists/reports.js
+++ lists/reports.js
@@ -5,9 +5,11 @@ function(head, req) {
     if (row.value.type == "report")
       d[row.value._id] = {"id": row.value._id, "created": row.value.created};
     if (row.value.type == "subreport" && row.value.subtype=="basic_template") {
-      d[row.value.report_id]['summary'] = (row.value['Summary']!=='' ? row.value['Summary'] : "");
-      d[row.value.report_id]['run'] = (row.value['Run number']!=='' ? row.value['Run number'] : "");
-      d[row.value.report_id]['crew'] = (row.value['Crew']!=='' ? row.value['Crew'] : "");
+      if(d[row.value.report_id]) {
+        d[row.value.report_id]['summary'] = (row.value['Summary']!=='' ? row.value['Summary'] : "");
+        d[row.value.report_id]['run'] = (row.value['Run number']!=='' ? row.value['Run number'] : "");
+        d[row.value.report_id]['crew'] = (row.value['Crew']!=='' ? row.value['Crew'] : "");
+      }
     }
   }
   var rows = [];
